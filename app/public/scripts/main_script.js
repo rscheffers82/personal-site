edb9745edd91016c49b5af818cebdaa00937f0db
Roy Scheffers <r.scheffers@gmail.com>
2016-12-02 03:55:46 -0500

    Add Sass support and revamp of the design

---
+++ app/public/scripts/main_script.js
@@ -0,0 +1,34 @@
+// Javascript
+$( document ).ready(function() {
+  // add all projects into portfolio part of the page
+
+});
+
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+$(".navbar a, footer a[href='#intro']").on('click', function(event) {
+  event.preventDefault();   // Prevent default anchor click behavior
+  var hash = this.hash;     // Store hash
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 900, function(){
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
+
+$('.projects').append( p.showProjects() );
+
+$( '.projects' ).on("click", 'a', function(e){
+  console.log( $(this).data('number') );
+  p.modal( $(this).data('number') );
+});
