3113e7feaf282531a9ead876badcc841b1e8a717
Roy Scheffers <r.scheffers@gmail.com>
2016-12-08 00:36:41 -0500

    Initial setup and Nav styled

--- app/js/main_script.js
+++ app/js/main_script.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
   // add all projects into portfolio part of the page
   // $('.projects').append( p.showProjects() );
-  // $(".sticky-wrapper").sticky({topSpacing:0});
+  $(".sticky-wrapper").sticky({topSpacing:0});
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
