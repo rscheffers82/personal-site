8e15dd6e10d38b60c6e6fe27f04dee7e924e180e
Roy Scheffers <r.scheffers@gmail.com>
2017-05-28 19:08:24 +0300

    Add Julie Cusmariu and Shortlnk project

--- app/js/project-data.js
+++ app/js/project-data.js
@@ -12,62 +12,69 @@ Format:
 var p = (function () {				// eslint-disable-line
 	var imgBaseDir = 'images/';
 	var projects = [{
+		'name': 'Brilliant Massage Therapy',
+		'description' : 'This is a client project in which I merged two sites into one in collaboration with Andrew from <a href="http://www.thinkupdesign.ca" target="_blank">ThinkUp Design</a>. After having done market research regarding local massage businesses and competing WordPress templates, we presented the client with the possible designs. The <a href="http://jacqueline.themerex.net" target="_blank">Jacqueline theme</a> was chosen.<br/> I migrated the current blog to a new WordPress site and installed the theme. After having styled most of the content and the revolution slider, Andrew took care of the intricate design fixes. The final result; a professional, SEO optimised, mobile ready and visual appealing site.',
+		'url': ['http://jolitabrilliant.com'],
+		'img' : 'jolita-brilliant.jpg',
+		'tech' : ['WordPress','Photoshop','CSS','HTML'],
+		'category' : ['featured', 'wordpress']
+	},{
+		'name': 'Cusmariu Life Coaching',
+		'description' : 'For this project Julie asked me to upgrade her static website to a dynamic and responsive WordPress site. As she already had extensive content on her previous site, specifically in relation to podcast interviews, the challenge was to present her services and content in a much clearer way.<br />To achieve this, a large part of the work involved creating various page and post templates files. Her new website runs on the <a href="https://demo.woothemes.com/canvas" target="_blank">Canvas parent theme</a> with the child theme that is heavily customized. Besided web development, I also assisted Julie with valuable insights into how she can better engage her audience which resulted in large changes to the home, about and work with me pages.',
+		'url': ['https://http://www.juliecusmariu.com/','https://github.com/rscheffers82/wordpress-custom-design'],
+		'img' : 'julie-cusmariu-project.jpg',
+		'tech' : ['WordPress', 'PHP', 'Photoshop','CSS','HTML'],
+		'category' : ['featured', 'wordpress']
+	},{
+		'name': 'Meteor Short Lnk',
+		'description' : '',
+		'url': ['https://rs-lnk.herokuapp.com','https://github.com/rscheffers82/short-lnk-meteor'],
+		'img' : 'short-lnk-meteor-project-roy-scheffers.jpg',
+		'tech' : ['Meteor', 'React', 'JSX', 'Sass','HTML'],
+		'category' : ['featured', 'meteor', 'react', 'backend', 'fullstack']
+	},{
 		'name': 'React List',
 		'description' : 'A personal project used to sharpen my skills in React and Redux. This app lets you add tasks to a list and search existing to-do items. Completed tasks are automatically archived and can be made visible again with the Show completed select box. Prior to this version, data was stored locally in the browser. Now, data is stored in Firebase using google authentication.',
 		'url': ['http://list11.herokuapp.com','https://github.com/rscheffers82/React-List-Application'],
 		'img' : 'react-to-do-app.jpg',
 		'tech' : ['React', 'Redux','JSX','Sass','HTML', 'Foundation'],
-		'category' : ['featured', 'frontend']
+		'category' : ['featured', 'react', 'frontend']
 	},{
 		'name': 'Meteor Score Keep',
 		'description' : 'In order to explore Meteor and get familiar with its eco-system, I build this project. The requirement was to create a database that records players and their score. On the front-end, players can be added and removed and their score increased and decreased. Animations are added using the React Flip Move library. Once completed, the app was deployed to Heroku with the help of the meteor horse build pack.',
 		'url': ['https://score-keep11.herokuapp.com','https://github.com/rscheffers82/score-keeper'],
 		'img' : 'score-keep.jpg',
 		'tech' : ['Meteor', 'React', 'JSX', 'Sass','HTML'],
-		'category' : ['featured', 'fullstack', 'backend']
+		'category' : ['meteor', 'react']
 	},{
 		'name': 'Five API projects',
 		'description' : 'This is one of my pet projects used to become familiar with Node, Express and MongoDB. I build five API apps that take input through either POST or GET and return JSON data. My favorite project was the Exercise Tracker. Here users can register, track their exercises and view logs. I added a front-end UI using Materialize CSS, although the Add user and Add Exercise POST routes will return JSON data for demonstration purposes.',
 		'url': ['https://rs82.herokuapp.com/','https://github.com/rscheffers82/API-microservices'],
 		'img' : 'API-collection.jpg',
 		'tech' : ['Node','Express','MongoDB', 'JavaScript', 'MaterializeCSS'],
-		'category' : ['featured','backend']
-	},{
-		'name': 'Brilliant Massage Therapy',
-		'description' : 'This is a client project in which I merged two sites into one in collaboration with Andrew from <a href="http://www.thinkupdesign.ca" target="_blank">ThinkUp Design</a>. After having done market research regarding local massage businesses and competing WordPress templates, we presented the client with the possible designs. The <a href="http://jacqueline.themerex.net" target="_blank">Jacqueline theme</a> was chosen.<br/> I migrated the current blog to a new WordPress site and installed the theme. After having styled most of the content and the revolution slider, Andrew took care of the intricate design fixes. The final result; a professional, SEO optimised, mobile ready and visual appealing site.',
-		'url': ['http://jolitabrilliant.com'],
-		'img' : 'jolita-brilliant.jpg',
-		'tech' : ['WordPress','Photoshop','CSS','HTML'],
-		'category' : ['featured', 'design']
-	},{
-		'name': 'React Countdown App',
-		'description' : 'This is one of my personal projects used to become familiar with re-using React components. In this timer app the countdown and timer parts of the application use the same components but slightly different logic is used in each. Building this app helped me to understand how props are passed down to child components and how application states are managed by a main parent container component. The application is styled using the <a href="foundation.zurb.com" target="_blank">Foundation</a> framework and deployed live on Heroku.',
-		'url': ['http://blooming-brushlands-69434.herokuapp.com','https://github.com/rscheffers82/ReactTimerApp'],
-		'img' : 'react-timer-app.jpg',
-		'tech' : ['React','JSX','Sass','HTML', 'Foundation'],
-		'category' : ['frontend']
+		'category' : ['featured', 'api', 'backend']
 	},{
 		'name': 'Black Belt Proofreader',
 		'description' : 'This project was built in collaboration with the business owner. Zorana started her own business as a legal transcript proofreader and needed a website to advertise her services. I provided her with the hosting and installed WordPress with the <a href="https://www.elegantthemes.com/gallery/divi" target="_blank">Divi theme</a>. I successfully implemented the overall styling and ensured that the site was supported by various mobile devices while she took care of the content and rough layout. All customization was saved in a child theme.',
 		'url': ['http://blackbeltproofreader.com'],
 		'img' : 'blackbeltproofreader.jpg',
 		'tech' : ['WordPress','Photoshop','CSS','HTML'],
-		'category' : ['featured', 'design']
-	},{
-		'name': 'React Weather App',
-		'description' : 'This is one of my first apps build on React. The application uses the Axios library to retrieve weather data from the <a href="http://openweathermap.org" target="_blank">Open Weather Map API</a> and shows the temperature based on the city provided. I use the React Router component to create a single window multi page app. The Get Weather page shows the weather based on the city entered. The Favorites page shows a list of cities which, when clicked, provide data through the URL to the main weather component and then displays the weather. Error handling is incorporated in case the API fails.',
-		'url': ['http://desolate-sierra-89528.herokuapp.com/','https://github.com/rscheffers82/ReactWeatherApp'],
-		'img' : 'react-weather-app.jpg',
-		'tech' : ['React','JSX','Sass','HTML','API'],
-		'category' : ['frontend']
+		'category' : ['featured', 'wordpress']
 	},{
 		'name': 'Simon',
 		'description' : 'This is my graduation project for the Front-end Program from <a href="https://www.freecodecamp.com" target="_blank">FreeCodeCamp</a>. Simon is an electronic retro game initially launched in 1978 after which it quickly became a pop culture symbol of the 70s and 80s.<br/>To recreate this game, I started of by creating the interface in Photoshop. Once done I transferred this to a simple page using HTML and Sass. The game logic was added using jQuery and JavaScript. For example, Simon will not accept input until it is switched on. By pressing start button a random sequence of 20 steps is generated and the first move is displayed by lighting up the corresponding color. The user much match this and as the game progresses the sequence becomes more complex. The game is eventually won after all 20 steps in the sequence are completed. This project helped me to program game logic into an application as well as make it look visually appealing on all devices.',
 		'url': ['https://rscheffers82.github.io/Simon-game/','https://github.com/rscheffers82/Simon-game'],
 		'img' : 'simon.jpg',
 		'tech' : ['JavaScript','Sass','HTML','Photoshop'],
-		'category' : ['featured', 'frontend', 'design']
+		'category' : ['featured', 'frontend']
 	},{
+		'name': 'React Countdown App',
+		'description' : 'This is one of my personal projects used to become familiar with re-using React components. In this timer app the countdown and timer parts of the application use the same components but slightly different logic is used in each. Building this app helped me to understand how props are passed down to child components and how application states are managed by a main parent container component. The application is styled using the <a href="foundation.zurb.com" target="_blank">Foundation</a> framework and deployed live on Heroku.',
+		'url': ['http://blooming-brushlands-69434.herokuapp.com','https://github.com/rscheffers82/ReactTimerApp'],
+		'img' : 'react-timer-app.jpg',
+		'tech' : ['React','JSX','Sass','HTML', 'Foundation'],
+		'category' : ['frontend', 'react']
+		},{
 		'name': 'Tic Tac Toe',
 		'description' : 'A classic Tic Tac Toe game that has two and single player game-play. Several levels of AI are build in that rely on the negaMax algorithm to calculate the score of future moves. It\'s one of my personal projects that I love and hate. I struggled quite a bit to understand the negaMax and MiniMax algorithms and how I can translate them into code that works. At the same time I enjoyed the larger functions I setup, which are used for each state of the game. Besides that I experimented with a modal to make it a single page app. The end result: a smooth game that entertains even the experienced Tic Tac Toe player',
 		'url': ['https://rscheffers82.github.io/Tic-Tac-Toe-JS-game/','https://github.com/rscheffers82/Tic-Tac-Toe-JS-game'],
@@ -80,21 +87,28 @@ var p = (function () {				// eslint-disable-line
 		'url': ['https://codepen.io/roycode/full/rLyZgm','https://github.com/rscheffers82/Pomodoro-Timer'],
 		'img' : 'sand-timer.jpg',
 		'tech' : ['JavaScript','CSS','HTML','HTML Canvas'],
-		'category' : ['featured', 'frontend']
+		'category' : ['frontend']
+	},{
+		'name': 'React Weather App',
+		'description' : 'This is one of my first apps build on React. The application uses the Axios library to retrieve weather data from the <a href="http://openweathermap.org" target="_blank">Open Weather Map API</a> and shows the temperature based on the city provided. I use the React Router component to create a single window multi page app. The Get Weather page shows the weather based on the city entered. The Favorites page shows a list of cities which, when clicked, provide data through the URL to the main weather component and then displays the weather. Error handling is incorporated in case the API fails.',
+		'url': ['http://desolate-sierra-89528.herokuapp.com/','https://github.com/rscheffers82/ReactWeatherApp'],
+		'img' : 'react-weather-app.jpg',
+		'tech' : ['React','JSX','Sass','HTML','API'],
+		'category' : ['featured', 'react', 'api']
 	},{
 		'name': 'Calculator',
 		'description' : 'A solar calculator styled using CSS and made functional by utilizing jQuery and JavaScript. It was a pretty straigh forward project that helped me greatly to understand CSS better and how to layout various element on the screen. Because of the many input elements, I used jQuery to catch input and various functions to perform the calculations.',
 		'url': ['http://codepen.io/roycode/full/qNapgw','https://github.com/rscheffers82/calculator-html-css-js'],
 		'img' : 'calculator-js.jpg',
 		'tech' : ['JavaScript','CSS','HTML','Photoshop'],
-		'category' : ['featured', 'frontend']
+		'category' : ['frontend']
 	},{
 		'name': 'Weather App',
 		'description' : 'A weather application which, based on the user\'s IP or browser information, displays the local weather using the <a href="https://darksky.net/dev" target="_blank">Dark Sky API</a>. This was a simple API project that I loved building, due to the creativity I could put into it. The various weather conditions each have their own background and the weather icon changes based on the current weather. The location is retrieved in multiple ways depending on the available information, either via browser info or via <a href="http://ip-api.com" target=_blank">ip-api.com</a>.',
 		'url': ['http://codepen.io/roycode/full/WwqYrB','http://codepen.io/roycode/pen/WwqYrB'],
 		'img' : 'Weather-application-in-javascript.jpg',
 		'tech' : ['JavaScript','Sass','HTML','Bootstrap','API'],
-		'category' : ['frontend']
+		'category' : ['frontend', 'api']
 	}
 	// ,{
 	// 	'name': 'Wikimedia Viewer',
